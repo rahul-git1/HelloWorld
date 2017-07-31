77996399904cc85f053b3782808e3c54e73212b9
Ubuntu <ubuntu@ip-172-31-2-94.ec2.internal>
2017-07-31 14:00:40 +0000

    blah blah blah blah

---
+++ git.sh
@@ -0,0 +1,8 @@
+echo "*************************"  >> readme.txt
+ date >> readme.txt
+echo "Hello all.....!!!!!!" >> readme.txt
+echo "*************************"  >> readme.txt
+git add readme.txt
+git commit -m " second email -- gmail is addded for email notification"
+git status
+git push origin master
