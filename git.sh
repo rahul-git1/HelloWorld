5e8e3536d6a0109415394e430a6177ffcbbfa880
root <rahul.drk@gmail.com>
2018-03-17 18:15:43 +0000

    removed all

--- git.sh
+++
@@ -1,8 +0,0 @@
-echo "*************************"  >> readme.txt
- date >> readme.txt
-echo "Hello all.....!!!!!!" >> readme.txt
-echo "*************************"  >> readme.txt
-git add readme.txt
-git commit -m " second email -- gmail is addded for email notification"
-git status
-git push origin master
